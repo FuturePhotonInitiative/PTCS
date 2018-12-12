e93285d3733c3ff78de58d50c6051b166777128d
Anthony Copeland <anthony.copeland21@gmail.com>
2018-12-12 14:03:50 -0500

    Edits for voltage source testing

--- src/Prober.py
+++ src/Prober.py
@@ -1,12 +1,14 @@
 import json
 import sys
 import pyvisa
+import imp
 
 
 def attach_VISA(manager, name, default):
 	"""
 	Attaches device in VISA format
 	:param manager: A pyVISA resource manager
+	:param name: something
 	:param default: The default address to connect to or None
 	:return: A pyVISA device
 	"""
@@ -14,7 +16,7 @@ def attach_VISA(manager, name, default):
 	if default:
 		for item in instruments.values():
 			if str(item.resource_name) == default or item.alias is not None and str(item.alias) == default:
-				return manager.open_resource(default)
+				return manager.open_resource(default, write_termination='\n', read_termination='\n')
 		print("The default port " + default + " for " + name + " could not be found, please select it manually")
 	else:
 		print("The default port for " + name + " was not specified, please choose one manually")
@@ -27,7 +29,7 @@ def attach_VISA(manager, name, default):
 		print(line)
 	print("***************************")
 	instr = raw_input("Choose instrument to connect to: ")
-	return manager.open_resource(instr)
+	return manager.open_resource(instr, read_termination='\n')
 
 
 def main():
@@ -56,8 +58,15 @@ def main():
 	for key in devices.keys():
 		print key
 	# device = attach_VISA(manager, config['Requires'].get('Default', None))
+	# Run filepath test
+	print "Importing file path..."
+	testfile = imp.load_source('voltage_test', config['TestScript']['Filepath'])
+	testfile.run(devices)
 
-
+	# typ = config['Requires']['Devices']['Type']
+	# example = imp.load_source(typ, "SPAE\\src\\Instruments\\"+typ+".py")
+	# with example.AgilentE3643A(devices[config['Requires']['Devices']['Name']]) as device:
+	#  		device.who_am_i()
 
 
 if __name__ == '__main__':
