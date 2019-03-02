b1b805807d5ba9dc9e56b28346ba41b9a35ece50
wxs5474 <wxs5474@rit.edu>
2019-03-02 14:54:29 -0500

    working on results page

--- src/GUI/UI/ExperimentResults/ResultsFileListPanel.py
+++ src/GUI/UI/ExperimentResults/ResultsFileListPanel.py
@@ -12,8 +12,6 @@ class ResultsFileListPanel(wx.ListBox):
         self.SetBackgroundColour(GUI_CONSTANTS.LIST_PANEL_COLOR)
         self.SetForegroundColour(GUI_CONSTANTS.LIST_PANEL_FOREGROUND_COLOR)
         # self.AppendColumn(QUEUE_PANEL_NAME)
-        for experiment in Globals.ExperimentQueue.get_experiment_names():
-            self.Append(experiment)
 
         self.Bind(wx.EVT_KEY_DOWN, self.return_to_main_control)
         self.Bind(wx.EVT_LISTBOX_DCLICK, self.return_to_main_control)
@@ -21,16 +19,13 @@ class ResultsFileListPanel(wx.ListBox):
 
     def return_to_main_control(self, event):
         # self.GetParent().render_control_box_with_experiment(None)
-        if (not isinstance(event, wx.KeyEvent)) or event.GetKeyCode() == wx.WXK_ESCAPE:
-            for selected in self.GetSelections():
-                self.Deselect(selected)
+        pass
 
     def on_hardware_select(self, event):
         # selected_experiment = Globals.ExperimentQueue.get_ith_experiment(self.GetSelection())
         # self.GetParent().render_control_box_with_experiment(selected_experiment)
         pass
 
-    def reload_panel(self):
+    def render_panel(self, experiment):
         self.Clear()
-        for experiment in Globals.ExperimentQueue.get_experiment_names():
-            self.Append(experiment)
+        self.experiment = experiment
--- src/GUI/UI/ExperimentResults/ResultsPage.py
+++ src/GUI/UI/ExperimentResults/ResultsPage.py
@@ -1,12 +1,13 @@
 import wx
-import ExperimentListPanel
+from ExperimentListPanel import ExperimentListPanel
+from ResultsFileListPanel import ResultsFileListPanel
 
 
 class ResultsPage(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        self.ExperimentListBox = ExperimentListPanel.ExperimentListPanel(self)
-        self.ResultsListBox = wx.StaticBox(self)
+        self.ExperimentListBox = ExperimentListPanel(self)
+        self.ResultsListBox = ResultsFileListPanel(self)
 
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         sizer.Add(self.ExperimentListBox, 1, wx.EXPAND)
