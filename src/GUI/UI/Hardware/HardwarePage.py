d9ffd53cbbdc6c7feb82470bf2a8adfcb7654f0c
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-03-05 13:56:27 -0500

    Adds label to Hardware Page

--- src/GUI/UI/Hardware/HardwarePage.py
+++ src/GUI/UI/Hardware/HardwarePage.py
@@ -16,8 +16,11 @@ class HardwarePage(wx.Panel):
         self.hardwareBox.SetForegroundColour(GUI_CONSTANTS.LIST_PANEL_FOREGROUND_COLOR)
 
         sizer = wx.BoxSizer(wx.HORIZONTAL)
+        right_side = wx.BoxSizer(wx.VERTICAL)
+        right_side.Add(wx.StaticText(self, label="Available Hardware"), CONSTANTS.QUEUE_LABEL_PROPORTION, wx.TOP)
+        right_side.Add(self.hardwareBox, 1, wx.EXPAND | wx.ALL)
         sizer.Add(self.controlBox, CONSTANTS.HARDWARE_CONTROL_PANEL_PROPORTION, wx.EXPAND, CONSTANTS.LIST_PANEL_MARGIN)
-        sizer.Add(self.hardwareBox, CONSTANTS.HARDWARE_PANEL_PROPORTION, wx.EXPAND, CONSTANTS.LIST_PANEL_MARGIN)
+        sizer.Add(right_side, CONSTANTS.HARDWARE_PANEL_PROPORTION, wx.EXPAND, CONSTANTS.LIST_PANEL_MARGIN)
 
         self.SetSizer(sizer)
         sizer.Layout()
