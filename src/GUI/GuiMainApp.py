963aa664132503216a29956ddf992a2298ee0843
wxs5474 <wxs5474@rit.edu>
2019-03-22 16:58:28 -0400

    Updating for config manager

--- src/GUI/GuiMainApp.py
+++ src/GUI/GuiMainApp.py
@@ -1,5 +1,6 @@
 import wx
 
+from src.GUI.Application.SystemConfigManager import SystemConfigManager
 from src.GUI.Model.ExperimentModel import Experiment
 from src.GUI.Model.ExperimentQueueModel import ExperimentQueueModel
 from src.GUI.Model.HardwareModel import HardwareModel
@@ -9,14 +10,8 @@ import src.GUI.Util.Globals as Globals
 
 if __name__ == '__main__':
     # TODO hardcoded config paths are bad and we can do better.
-    Globals.ExperimentQueue = ExperimentQueueModel(['../../System/Devices.json', '../../System/Files.json'])
-    Globals.Hardware = HardwareModel(['../../System/Devices.json', '../../System/Files.json'])
+    Globals.systemConfigManager = SystemConfigManager('../../System/Files.json')
     # TODO temp testing code to make sure queue displays properly
-    Globals.ExperimentQueue.add_to_queue(Experiment("../../Configs/Dummy_Test1.json"))
-    Globals.ExperimentQueue.add_to_queue(Experiment("../../Configs/Dummy_Test2.json"))
-    Globals.ExperimentQueue.add_to_queue(Experiment("../../Configs/Dummy_Test3.json"))
-    Globals.ExperimentQueue.add_to_queue(Experiment("../../Configs/Dummy_Test4.json"))
-    Globals.ExperimentQueue.add_to_queue(Experiment("../../Configs/Dummy_Test5.json"))
     app = wx.App()
     frame = MainFrame(None, -1)
     frame.Show()
