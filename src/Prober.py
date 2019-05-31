c8a8621cd91d3c1591d903d8da5409d35e04eb54
Mark Nash <mdn4993@rit.edu>
2019-05-31 10:18:05 -0400

    Moved all device setup logic to another file

--- src/Prober.py
+++ src/Prober.py
@@ -1,95 +1,12 @@
 import inspect
-import json
 import sys
 import types
-import pyvisa
-import os
 import contextlib2
 import imp
 
 from Probe.Args import Args
 from Probe.ConfigFileManipulation import ConfigFileManipulation
-
-instruments = None
-
-
-def attach_VISA(manager, name, default):
-    """
-    Attaches device in VISA format, will scan for devices once because it is a slow process, caches results for future
-        use. This will attempt to connect to a default address if it is specified in the JSON config, otherwise it will
-        prompt the user to input an address from the list of currently connected devices.
-    :param manager: A pyVISA resource manager
-    :param name: The name of the device being connected
-    :param default: The default address to connect to or None
-    :return: A pyVISA device
-    """
-    global instruments
-    if instruments is None:
-        instruments = manager.list_resources_info()
-
-    if default:
-        for item in instruments.values():
-            if str(item.resource_name) == default or item.alias is not None and str(item.alias) == default:
-                return manager.open_resource(default)
-        print("The default port " + default + " for " + name + " could not be found, please select it manually")
-    else:
-        print("The default port for " + name + " was not specified, please choose one manually")
-
-    print("*****Connected Devices*****")
-    for item in instruments.values():
-        line = item.resource_name
-        if item.alias is not None:
-            line += " -> '" + item.alias + "'"
-        print(line)
-    print("***************************")
-    instr = raw_input("Choose instrument to connect to: ")
-    return manager.open_resource(instr)
-
-
-def connect_devices(config_file, file_locations, exit_stack):
-    """
-    Initializes all devices specified in the JSON config, this will also dynamically import the drivers specified if one
-        hasn't been imported already
-    :param config_file: The JSON config object
-    :param file_locations: The JSON Files object with standard file directories
-    :param exit_stack: A Exit Stack that will close all devices when the program exits
-    :return: A dict of device names mapping to their objects
-    """
-    manager = pyvisa.ResourceManager()
-    driver_root = str(file_locations['Driver_Root'])
-    drivers = os.listdir(driver_root)
-    drivers = [i[:-3] for i in drivers if not (i == '__init__.py' or i[-3:] != '.py')]
-    devices = {}
-
-    print "Finding Devices...."
-
-    with open(file_locations["Hardware_Config"]) as d:
-        hardware_manager = json.load(d)
-    # this one is updated for the hardware manager
-    for dev in config_file['Devices']:
-        if dev not in hardware_manager.keys():
-            print "Device not found in Devices.json: "+dev
-        else:
-            connection = None
-            device = hardware_manager[dev]
-            print device
-            if str(device['Type']) == "VISA":
-                connection = attach_VISA(manager, str(dev), str(device['Default']))
-            elif str(device['Type']) == "DIRECT" and "Default" in device.keys():
-                connection = str(device['Default'])
-            else:
-                connection = raw_input(
-                    "\'" + str(dev) + "\' cannot be used with VISA, Please enter connection info (eg. IP address): ")
-
-            driver = str(device['Driver'])
-            if driver in drivers:
-                if driver not in [i[0] for i in globals().items() if isinstance(i[1], types.ModuleType)]:
-                    globals()[driver] = imp.load_source(driver, driver_root + '\\' + driver + '.py')
-                devices[str(dev)] = exit_stack.enter_context(inspect.getmembers(globals()[driver],
-                                                                                inspect.isclass)[0][1](connection))
-            else:
-                sys.exit("Driver file for \'" + driver + "\' not found in Driver Root: \'" + driver_root)
-    return devices
+from Probe.DeviceSetup import DeviceSetup
 
 
 def spawn_scripts(scripts, data_map, json_locations, experiment_result):
@@ -117,10 +34,13 @@ def spawn_scripts(scripts, data_map, json_locations, experiment_result):
     return
 
 
-def main(args, config_manager=None, queue_result=None, logger = None):
+def main(args, config_manager=None, queue_result=None):
     """
-    Entry point of SPAE, loads config file
-    :param args: test configuration file
+    Entry point of SPAE.
+    Loads config file, parses parameters, sets up the devices, and runs the scripts specified.
+    :param args: run Prober.py with no arguments to see the argument specification
+    :param config_manager: the ConfigurationManager object to obtain configuration data for running the scripts
+    :param queue_result: the QueueResult object to add run result data to
     :return: None
     """
     from GUI.Application.SystemConfigManager import SystemConfigManager
@@ -157,9 +77,12 @@ def main(args, config_manager=None, queue_result=None, logger = None):
 
     scripts = config.extract_scripts(config_manager.file_locations)
 
-    # autocloses devices if an error occurred
     with contextlib2.ExitStack() as stack:
-        data_map['Devices'] = connect_devices(config.config, config_manager.file_locations, stack)
+        device_setup = DeviceSetup()
+        data_map['Devices'] = device_setup.connect_devices(
+            config.config["Devices"],
+            config_manager.file_locations,
+            stack)
         spawn_scripts(scripts, data_map, config_manager.file_locations, experiment_result)
 
     experiment_result.end_experiment()
