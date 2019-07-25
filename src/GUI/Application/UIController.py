b52d78f95448cbce0f4df6612622e40a6cd07a84
Mark Nash <mdn4993@rit.edu>
2019-07-25 09:09:29 -0400

    The non-gui config file running now works

--- src/GUI/Application/UIController.py
+++ src/GUI/Application/UIController.py
@@ -1,8 +1,8 @@
 import sys
+import wx
 
 from src.GUI.UI.Queue.ExperimentControlPanel import ExperimentControlPanel
 from src.GUI.UI.Queue.ExperimentOutputPanel import ExperimentOutputPanel
-from src.GUI.Util.Functions import fix_text_size
 
 
 class UIController:
@@ -37,7 +37,7 @@ class UIController:
     def fix_control_list(self):
         for control in self.controls_to_fix_text_size:
             # print control
-            fix_text_size(control, 10)
+            UIController.fix_text_size(control, 10)
 
     def switch_to_result(self):
         # self.mainframe.notebook
@@ -56,3 +56,26 @@ class UIController:
     def switch_queue_to_edit(self):
         self.mainframe.queue_page.set_control(ExperimentControlPanel(self.mainframe.queue_page))
         self.mainframe.queue_page.control_panel.set_up_ui_control(self)
+
+    @staticmethod
+    def fix_text_size(control, margin):
+        try:
+            font = control.GetFont()
+            # print font
+            # size = wx.Size(control.GetSize().Get()[1] / 4, control.GetSize().Get()[1] / 4)
+            # print size
+            wx.Font.Scale(font, round((float(control.GetSize().Get()[1] - margin) /
+                                       float(font.GetPixelSize()[1])) / 2.0))
+            # wx.Font.SetPixelSize(font, size)
+            dc = wx.ScreenDC()
+            dc.SetFont(font)
+            w, h = dc.GetTextExtent("test string")
+            if w + margin > control.GetSize().Get()[0] and \
+                    control.GetSize().Get()[0] * (float(control.GetSize().Get()[0] - margin) / float(w)) > 0:
+                # print 2
+                scale = round(float(control.GetSize().Get()[0] - margin) / float(w), 2)
+                wx.Font.Scale(font, scale)
+                # print scale
+            control.SetFont(font)
+        except Exception as e:
+            pass
