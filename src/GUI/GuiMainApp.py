ec51f767e245073f5468bf352467e93e125c4d7a
Mark Nash <mdn4993@rit.edu>
2019-07-29 15:39:41 -0400

    now can be run standalone

--- src/GUI/GuiMainApp.py
+++ src/GUI/GuiMainApp.py
@@ -1,15 +1,18 @@
 import wx
 
-from src.GUI.Application.SystemConfigManager import SystemConfigManager
-from src.GUI.UI.MainFrame import MainFrame
-import src.GUI.Util.Globals as Globals
+import sys
+import os
 
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 if __name__ == '__main__':
+    from Application.SystemConfigManager import SystemConfigManager
+    from UI.MainFrame import MainFrame
+    import src.GUI.Util.Globals as Globals
+
     Globals.systemConfigManager = SystemConfigManager()
     app = wx.App()
     frame = MainFrame(None, -1)
     Globals.systemConfigManager.mainframe = frame
     frame.Show()
     app.MainLoop()
-
