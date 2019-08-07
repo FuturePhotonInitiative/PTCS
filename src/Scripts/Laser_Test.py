cc76c6929be92cd75165ac9ecf7acf41ddb6f959
ofs9424 <ofs9424@rit.edu>
2019-08-07 16:09:06 -0400

    Added parameters to Laser Test.

--- src/Scripts/Laser_Test.py
+++ src/Scripts/Laser_Test.py
@@ -3,8 +3,13 @@ def main(data_map, experiment_result):
 	data_map['Data']['Collect'] = {}
 	pd = data_map['Devices']['Newport OPM']
 	ls = data_map['Devices']['Laser Source']
+	start = data_map['Data']['Initial']['start_wavelength']
+	end = data_map['Data']['Initial']['end_wavelength']
+	w_step = data_map['Data']['Initial']['step_wavelength']
+	t_step = data_map['Data']['Initial']['time_step']
+	
 	pd.make_outputs_unverbose()
-	ls.run_sweep_step(1520, 1530, 1, 2)
+	ls.run_sweep_step(start, end, w_step, t_step)
 	start_time = time.time()
 	t = (time.time() - start_time)
 	ct = 5
