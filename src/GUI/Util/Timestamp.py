237c0a10a3320d83e3a6a3fef7df154cc5c0268c
Mark Nash <mdn4993@rit.edu>
2020-01-23 15:02:02 -0500

    documentation to Timestamp class

--- src/GUI/Util/Timestamp.py
+++ src/GUI/Util/Timestamp.py
@@ -1,10 +1,14 @@
 import datetime
 from src.GUI.Util.CONSTANTS import TIMESTAMP_FORMAT
 
+# this is what the timestamp will be formatted as if one wants to put the timestamp in a file name
 FORMAT_FOR_FILE_NAME = "_y%Y_m%m_d%d_h%H_m%M_s%S_us%f"
 
 
 class Timestamp:
+    """
+    A class to foster code reuse when dealing with timestamps throughout the application
+    """
 
     def __init__(self):
         self._stamp = datetime.datetime.today()
@@ -16,6 +20,9 @@ class Timestamp:
         return instance
 
     def for_filename(self) -> str:
+        """
+        :return: a filename-safe representation of the timestamp
+        """
         return self._stamp.strftime(FORMAT_FOR_FILE_NAME)
 
     def __str__(self):
